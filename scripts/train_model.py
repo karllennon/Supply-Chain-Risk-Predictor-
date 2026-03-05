b80159ed2c9fc9180bbbedeb352855c1adc3cdb9
karllennon <karlgodel32@gmail.com>
2026-03-05 14:58:02 -0800

    Clean column names in training data
    Replace spaces and special characters in column names.

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -25,6 +25,7 @@ X = df[features]
 y = df[target]
 
 X = pd.get_dummies(X, drop_first=True)
+X.columns = [c.replace(' ', '_').replace('[', '').replace(']', '').replace('<', '') for c in X.columns]
 
 # 3. Train/Test Split (80/20)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
